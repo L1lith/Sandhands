11648268b87e5556e5dd8fdebedf232db95304a0
L1lith <lily@lillith.pw>
2018-06-20 17:22:49 -0600

    fix router

--- docs/router.js
+++ docs/router.js
@@ -1,14 +1,16 @@
 import React, {Component} from 'react'
-import {Switch, Router} from 'react-router-dom'
+import {Switch, Route} from 'react-router-dom'
 import {join} from 'path'
 
-const routes = require.context(join(__dirname, '/pages'), true, /\.js$/)
+const required = require.context('./pages/', true, /\.js$/)
+
+const routes = required.keys().map(key => required(key)).map(module => module.__esModule === true ? module.default || {} : module)
 
 console.log(routes)
 
 export default (
   <Switch>
-    {routers.map((options, index) => (
+    {routes.map((options, index) => (
       <Route index={index} {...options} />
     ))}
   </Switch>
