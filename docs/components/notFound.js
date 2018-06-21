c896be3568a7647180aa622dc6aa0c6f58fcc521
L1lith <lily@lillith.pw>
2018-06-20 20:09:57 -0600

    update not found page

--- docs/components/notFound.js
+++ docs/components/notFound.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react'
+import {Link} from 'react-router-dom'
 
 class NotFound extends Component {
   render() {
     return (
       <div className="not-found">
-        <h1>404</h1>
-        <h2>Page Not found</h2>
+        <h1 className="status-code">404</h1>
+        <h2 className="status-text">Page Not found</h2>
+        <h1 className="return"><Link to="/">Return Home</Link></h1>
       </div>
     )
   }
