1769bf4ff592c2ac85fed979980cb6e6da516449
L1lith <lily@lillith.pw>
2018-06-20 19:28:34 -0600

    update header

--- docs/components/header.js
+++ docs/components/header.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react'
+import {Link} from 'react-router-dom'
 
 class Header extends Component {
   render() {
     return (
       <header className="main">
-        <h1 className="sitetitle">Sandhands Documentation</h1>
+        <h1 className="sitetitle"><Link to="/">Sandhands Documentation</Link></h1>
       </header>
     )
   }
