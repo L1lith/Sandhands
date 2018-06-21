2c77df80d6b7700bbbe6cd828f9a23ee466e0362
L1lith <lily@lillith.pw>
2018-06-20 19:40:23 -0600

    update header

--- docs/components/header.js
+++ docs/components/header.js
@@ -6,6 +6,11 @@ class Header extends Component {
     return (
       <header className="main">
         <h1 className="sitetitle"><Link to="/">Sandhands Documentation</Link></h1>
+        <ul>
+          {[["Github", "https://github.com/l1lith/sandhands"]].map(([text, link], index) => (
+            <li><Link key={index} to={link}>{text}</Link></li>
+          ))}
+        </ul>
       </header>
     )
   }
