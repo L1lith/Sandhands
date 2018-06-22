cb4c9bf8c7a17cc28432163376f7d24a0728cddd
L1lith <lily@lillith.pw>
2018-06-21 23:18:47 -0600

    update sandbox

--- docs/pages/sandbox.js
+++ docs/pages/sandbox.js
@@ -8,19 +8,21 @@ import titleCase from '@functions/titleCase'
 class Sandbox extends Component {
   constructor(props) {
     super(props)
-    this.state = {editors: ["input", "format"], messages: {}, valid: {}, values: {}};
+    this.state = {editors: ["input", "format"], messages: {}, valid: {}, values: {}, output: {valid: null, sanitize: null}};
     ['setValid','handleChange','onValid', 'setMessage', 'onError'].forEach(prop => this[prop] = this[prop].bind(this))
   }
   render() {
     return (
       <div className="sandbox">
-        {this.state.editors.map( (name, index) => (
-          <div key={index} className="editor">
-            <h2 className="name">{titleCase(name)}</h2>
-            <AceEditor value={this.state.values[name] || ""} className="input" mode="javascript" theme="ambiance" onChange={(...args)=>{this.handleChange(name, ...args)}}/>
-            <p>Valid {this.state.valid[name] === true ? "✔" : "✖"}</p>
-          </div>
-        ))}
+        <div className="editors">
+          {this.state.editors.map( (name, index) => (
+            <div key={index} className="editor">
+              <h2 className="name">{titleCase(name)}</h2>
+              <AceEditor value={this.state.values[name] || ""} className="input" mode="javascript" theme="ambiance" onChange={(...args)=>{this.handleChange(name, ...args)}}/>
+              <p>Valid Syntax {this.state.valid[name] === true ? "✔" : "✖"}</p>
+            </div>
+          ))}
+        </div>
       </div>
     )
   }
