edec87f65a060b6cb9d6eac0f6929e5d04ffa452
L1lith <lily@lillith.pw>
2018-06-21 23:01:27 -0600

    work on sandbox

---
+++ docs/functions/titleCase.js
@@ -0,0 +1,10 @@
+function titleCase(str) {
+    return str.replace(
+        /\w\S*/g,
+        function(txt) {
+            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+        }
+    );
+}
+
+export default titleCase
--- docs/pages/sandbox.js
+++ docs/pages/sandbox.js
@@ -3,43 +3,59 @@ import AceEditor from 'react-ace'
 import brace from 'brace'
 import 'brace/mode/javascript'
 import 'brace/theme/ambiance'
+import titleCase from '@functions/titleCase'
 
 class Sandbox extends Component {
   constructor(props) {
     super(props)
-    this.state = {theme: null}
-    window.setTheme = themeName => {
-      this.setState({theme: themeName})
-    }
+    this.state = {editors: ["input", "format"], messages: {}, valid: {}, values: {}};
+    ['setValid','handleChange','onValid', 'setMessage', 'onError'].forEach(prop => this[prop] = this[prop].bind(this))
   }
   render() {
     return (
       <div className="sandbox">
-        <div className="editor-titles">
-          <h2>Input Data</h2><h2>Format Object</h2>
-        </div>
-        <div className="editors">
-          <AceEditor className="input" mode="javascript" theme="ambiance" onChange={this.handleChange.bind(this, "input")}/>
-          <AceEditor className="input" mode="javascript" theme="ambiance" onChange={this.handleChange.bind(this, "format")}/>
-        </div>
+        {this.state.editors.map( (name, index) => (
+          <div key={index} className="editor">
+            <h2 className="name">{titleCase(name)}</h2>
+            <AceEditor value={this.state.values[name] || ""} className="input" mode="javascript" theme="ambiance" onChange={(...args)=>{this.handleChange(name, ...args)}}/>
+            <p>Valid {this.state.valid[name] === true ? "✔" : "✖"}</p>
+          </div>
+        ))}
       </div>
     )
   }
   handleChange(editor, value) {
+    const values = {...this.state.values}
+    values[editor] = value
+    this.setState({values})
     let object
     try {
-      object = eval("(" + value + ")")
+      this.onValid(editor, eval("(" + value + ")"))
     } catch(error) {
-      this.evalError(editor)
-      return
+      this.onError(editor, error)
     }
-    setObject(editor, object)
   }
-  evalError(editor) {
-
+  onError(editor, error) {
+    console.error(error)
+    this.setValid(editor, false)
   }
-  setObject(editor, object) {
-    
+  setValid(editor, isValid) {
+    console.log('sv', {editor, isValid, valid: this.state.valid})
+    const valid = {...this.state.valid}
+    valid[editor] = isValid === true
+    this.setState({valid})
+  }
+  onValid(editor, object) {
+    this.setValid(editor, true)
+  }
+  setMessage(editor, message) {
+    const newMessages = {...this.state.messages}
+    if (message === null) {
+      delete newMessages[editor]
+    } else {
+      newMessages[editor] = message
+    }
+    this.setState({messages: newMessages})
   }
 }
 
