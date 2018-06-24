37ab844c417b35c96309523fdaa5e41d6e05a5f6
L1lith <lily@lillith.pw>
2018-06-24 03:34:26 -0600

    remove test link

--- docs-source/pages/home.js
+++ docs-source/pages/home.js
@@ -1,12 +1,10 @@
 import React, {Component} from 'react'
-import {Link} from 'react-router-dom'
 
 class Home extends Component {
   render() {
     return (
       <div className="home">
         <p>Welcome to the Sandhands Documentation. Use the links above to get started</p>
-        <Link to="/sandhands/test">test</Link>
       </div>
     )
   }
