17a5385ccd2fa868ce013ac698da534a5442ec3b
L1lith <lily@lillith.pw>
2018-07-07 09:47:36 -0600

    add regex to allowed string options map

--- primitive.js
+++ primitive.js
@@ -13,7 +13,7 @@ const defaultOptions = new Map([
 ])
 
 const allowedOptions = new Map([
-  [String, ['allowed', 'banned', 'minLength', 'maxLength', 'lowercase', 'uppercase', 'equalTo', 'email', 'whitespace']],
+  [String, ['allowed', 'banned', 'minLength', 'maxLength', 'lowercase', 'uppercase', 'equalTo', 'email', 'whitespace', 'regex']],
   [Number, ['min', 'max', 'even', 'odd', 'allowNaN', 'finite', 'equalTo']],
   [Boolean, ['equalTo']]
 ])
