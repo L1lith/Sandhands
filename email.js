a3a2ec1cc0267d40357fd2751b4a6a07e56a9c55
L1lith <lily@lillith.pw>
2018-07-07 09:59:19 -0600

    remove unneccesary typeof check

--- email.js
+++ email.js
@@ -2,7 +2,7 @@
 const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
 function validEmail(data) {
-  return typeof data == 'string' && emailRegex.test(data)
+  return emailRegex.test(data)
 }
 
 module.exports = validEmail
