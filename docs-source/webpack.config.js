c1ea74e2372ed4b4d4188e7d22c1229a1e18ef1e
L1lith <lily@lillith.pw>
2018-07-08 21:42:43 -0600

    implement packenv

--- docs-source/webpack.config.js
+++ docs-source/webpack.config.js
@@ -1,6 +1,3 @@
-const merge = require('webpack-merge')
-const common = require("./webpack.common.js")
 process.env.NODE_ENV = process.env.NODE_ENV || "development"
-const environment = require(`./webpack.${process.env.NODE_ENV}.js`)
 
-module.exports = merge(common, environment)
+module.exports = require('packenv')()
