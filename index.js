7ae1f66160dff2b10a00c57eebd2d4c95568f522
L1lith <lily@lillith.pw>
2018-07-09 18:20:42 -0600

    move source code to it's own folder

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const sanitizeObject = require('./object')
-const valid = require('./valid')
-const sanitize = require('./sanitize')
-const Sanitizer = require('./Sanitizer')
+const sanitizeObject = require('./source/object')
+const valid = require('./source/valid')
+const sanitize = require('./source/sanitize')
+const Sanitizer = require('./source/Sanitizer')
 
 module.exports = Object.freeze({Sanitizer, sanitize, valid, details: sanitizeObject})
