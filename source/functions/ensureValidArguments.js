539480b7bf42f183bfcb65ad580961d57ab39e49
L1lith <lily@lillith.pw>
2018-07-24 15:06:40 -0600

    fix syntax error

--- source/functions/ensureValidArguments.js
+++ source/functions/ensureValidArguments.js
@@ -3,7 +3,7 @@ const validateAny = require('../validate/any')
 function ensureValidArguments(input, format, options) {
   if (arguments.length < 1) throw new Error('Missing Input Argument')
   if (arguments.length < 2) throw new Error('Missing Format Argument')
-  if (arguments.length > 2 && (typeof options != 'object' || options === null)) throw new error("Options Invalid")
+  if (arguments.length > 2 && (typeof options != 'object' || options === null)) throw new Error("Options Invalid")
   const error = validateAny(format, options)
   if (error !== null) throw new Error(error)
 }
