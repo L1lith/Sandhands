04ff71702f755908cb8138030304cc8d1cb79214
L1lith <lily@lillith.pw>
2018-07-24 14:05:24 -0600

    add allowedOptions file

---
+++ source/allowedOptions.js
@@ -0,0 +1,11 @@
+const universal = ["equalTo", "validate"]
+
+const primitives = new Map([
+  [String, ["regex", "minLength", "maxLength", "length", "allowed", "banned", "lowercase", "uppercase", "email", "whitespace"]],
+  [Number, ["allowNaN", "finite", "min", "max", "even", "odd"]]
+])
+
+const object = ["strict"]
+const array = ["strict", "firstAsStandard"]
+
+module.exports = {universal, primitives}
