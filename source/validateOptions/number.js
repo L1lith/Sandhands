b6a38be64298c00b51a2297b59d19497ab8bc417
L1lith <lily@lillith.pw>
2018-07-24 12:26:42 -0600

    update validate functions

--- source/validateOptions/number.js
+++ source/validateOptions/number.js
@@ -1,26 +1,25 @@
 function validateNumberOptions(options) {
-  const errors = []
   const {allowNaN, finite, min, max, even, odd} = options
 
   if (options.hasOwnProperty('allowNaN')) {
-    if (typeof allowNaN != 'boolean') errors.push('Invalid AllowNaN Option')
+    if (typeof allowNaN != 'boolean') return 'Invalid AllowNaN Option'
   }
   if (options.hasOwnProperty('finite')) {
-    if (typeof finite != 'boolean') errors.push('Invalid Finite Option')
+    if (typeof finite != 'boolean') return 'Invalid Finite Option'
   }
   if (options.hasOwnProperty('min')) {
-    if (typeof min != 'number' && !isFinite(min)) errors.push('Invalid Min Option')
+    if (typeof min != 'number' && !isFinite(min)) return 'Invalid Min Option'
   }
   if (options.hasOwnProperty('max')) {
-    if (typeof max != 'number' || !isFinite(max)) errors.push('Invalid Max Option')
+    if (typeof max != 'number' || !isFinite(max)) return 'Invalid Max Option'
   }
   if (options.hasOwnProperty('even')) {
-    if (typeof even != 'boolean') errors.push('Invalid Even Option')
+    if (typeof even != 'boolean') return 'Invalid Even Option'
   }
   if (options.hasOwnProperty('odd')) {
-    if (typeof odd != 'boolean') errors.push('Invalid Odd Option')
+    if (typeof odd != 'boolean') return 'Invalid Odd Option'
   }
-  return errors
+  return null
 }
 
 module.exports = validateNumberOptions
--- source/validateOptions/string.js
+++ source/validateOptions/string.js
@@ -1,38 +1,37 @@
 function validateStringOptions(options) {
-  const errors = []
   const {regex, minLength, maxLength, length, allowed, banned, lowercase, uppercase, email, whitespace} = options
 
   if (options.hasOwnProperty('email')) {
-    if (typeof email != 'boolean') errors.push('Invalid Email Option')
+    if (typeof email != 'boolean') return 'Invalid Email Option'
   }
   if (options.hasOwnProperty('minLength')) {
-      if (typeof minLength != 'number' || !isFinite(minLength) || minLength < 0) errors.push('Invalid Minimum Length Option')
+      if (typeof minLength != 'number' || !isFinite(minLength) || minLength < 0) return 'Invalid Minimum Length Option'
   }
   if (options.hasOwnProperty('maxLength')) {
-    if (typeof options.maxLength != 'number' || !isFinite(options.maxLength) || options.maxLength < 0) errors.push('Invalid Maximum Length Option')
+    if (typeof options.maxLength != 'number' || !isFinite(options.maxLength) || options.maxLength < 0) return 'Invalid Maximum Length Option'
   }
   if (options.hasOwnProperty('length')) {
-    if (typeof length != 'number' || !isFinite(length) || length < 0) errors.push('Invalid Length Option')
+    if (typeof length != 'number' || !isFinite(length) || length < 0) return 'Invalid Length Option'
   }
   if (options.hasOwnProperty('allowed')) {
-    if (typeof allowed != 'string' || allowed.length < 1) errors.push('Invalid Allowed Option')
+    if (typeof allowed != 'string' || allowed.length < 1) return 'Invalid Allowed Option'
   }
   if (options.hasOwnProperty('banned')) {
-    if (typeof banned != 'string' || banned.length < 1) errors.push('Invalid Banned Option'
+    if (typeof banned != 'string' || banned.length < 1) return 'Invalid Banned Option'
   }
   if (options.hasOwnProperty('lowercase')) {
-    if (typeof lowercase != 'boolean') errors.push('Invalid Lowercase Option')
+    if (typeof lowercase != 'boolean') return 'Invalid Lowercase Option'
   }
   if (options.hasOwnProperty('uppercase')) {
-    if (typeof uppercase != 'boolean') errors.push('Invalid Lowercase Option')
+    if (typeof uppercase != 'boolean') return 'Invalid Lowercase Option'
   }
   if (options.hasOwnProperty('whitespace')) {
-    if (typeof whitespace != 'boolean') errors.push('Invalid Whitespace Option')
+    if (typeof whitespace != 'boolean') return 'Invalid Whitespace Option'
   }
   if (options.hasOwnProperty('regex')) {
-    if (!(regex instanceof RegExp)) errors.push('Invalid Regex Option')
+    if (!(regex instanceof RegExp)) return 'Invalid Regex Option'
   }
-  return errors
+  return null
 }
 
 module.exports = validateStringOptions
