2618e66277f64ba5b7a8cf75d98da33ad76cbefa
L1lith <lily@lillith.pw>
2018-07-25 15:33:59 -0600

    update custom formats

--- source/customFormats.js
+++ source/customFormats.js
@@ -1,5 +1,5 @@
 module.exports = {
-  email: {_: String, email: true},
-  username: {_: String, regex: /^[a-zA-Z\-0-9]+$/, minLength: 3},
+  email: {_: String, email: true, lowercase: true},
+  username: {_: String, regex: /^[a-zA-Z\-0-9]+$/, minLength: 3, lowercase: true},
   password: {_: String, minLength: 8}
 }
