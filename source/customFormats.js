46b1c21da853ff96fa93c36e9e9c881fcacbb3ba
L1lith <lily@lillith.pw>
2018-07-25 15:29:52 -0600

    add username and password custom formats

--- source/customFormats.js
+++ source/customFormats.js
@@ -1,3 +1,5 @@
 module.exports = {
-  email: {_: String, email: true}
+  email: {_: String, email: true},
+  username: {_: String, regex: /^[a-zA-Z\-0-9]+$/, minLength: 3},
+  password: {_: String, minLength: 8}
 }
