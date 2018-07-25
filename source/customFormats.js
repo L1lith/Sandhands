07430e16ca5f337f5451c73df57f9a3e4b1e018e
L1lith <lily@lillith.pw>
2018-07-25 15:34:58 -0600

    update custom format

--- source/customFormats.js
+++ source/customFormats.js
@@ -1,5 +1,5 @@
 module.exports = {
   email: {_: String, email: true, lowercase: true},
-  username: {_: String, regex: /^[a-zA-Z\-0-9]+$/, minLength: 3, lowercase: true},
+  username: {_: String, regex: /^[a-zA-Z\-0-9]+$/, minLength: 3, lowercase: true, validate: str => !str.startsWith('-') && !str.endsWith('-')},
   password: {_: String, minLength: 8}
 }
