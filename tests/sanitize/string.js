a733b3e5362f1c75d4c6e34b36233e959a23fe50
L1lith <lily@lillith.pw>
2018-07-26 20:04:57 -0600

    configure test suite and make basic string test

---
+++ tests/sanitize/string.js
@@ -0,0 +1,16 @@
+const valid = require('../../source/exports/valid')
+const chai = require('chai')
+const {assert} = chai
+
+const expectedMatches = [
+  ['words', String, true, 'matches by default for a string longer than 1'],
+  ['', String, false, 'doesn\'t match by default for empty strings'],
+  ['words', {_: String, lowercase: true}, true, 'matches lowercase strings with the lowercase flag'],
+  ['WORDS', {_: String, lowercase: true}, false, 'doesn\'t match uppercase strings for the uppercase flag']
+]
+
+describe('String Matching', ()=>{
+  expectedMatches.forEach(([input, format, expectedResult, description])=>{
+    it(description, ()=> assert.equal(valid(input, format), expectedResult))
+  })
+})
