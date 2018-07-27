02a3ccdf60dc96f81b158e1e8ba5225cb279b7f1
L1lith <lily@lillith.pw>
2018-07-26 20:09:19 -0600

    update string tests

--- tests/sanitize/string.js
+++ tests/sanitize/string.js
@@ -6,7 +6,9 @@ const expectedMatches = [
   ['words', String, true, 'matches by default for a string longer than 1'],
   ['', String, false, 'doesn\'t match by default for empty strings'],
   ['words', {_: String, lowercase: true}, true, 'matches lowercase strings with the lowercase flag'],
-  ['WORDS', {_: String, lowercase: true}, false, 'doesn\'t match uppercase strings for the uppercase flag']
+  ['SANDWICH', {_: String, lowercase: true}, false, 'doesn\'t match uppercase strings for the uppercase flag'],
+  ['SALMON', {_: String, uppercase: true}, true, 'matches uppercase strings with the uppercase flag'],
+  ['tuna', {_: String, uppercase: true}, false, 'doesn\'t match lowercase strings for the uppercase flag']
 ]
 
 describe('String Matching', ()=>{
