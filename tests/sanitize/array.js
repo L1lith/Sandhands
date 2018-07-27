cb5ac89ff7e1f656aa4937dfdbf0c7031781f2a6
L1lith <lily@lillith.pw>
2018-07-27 10:59:05 -0600

    add more tests

--- tests/sanitize/array.js
+++ tests/sanitize/array.js
@@ -7,6 +7,8 @@ const expected = [
   [[234234, 63452, 52523, 'Hamburgers'], [Number], false, 'any length of non-matching array to an array with a length of one', {spreadArray: false}],
   [[424, 234324], [Number, Number], true, 'a valid array with equal length to an array longer than 1', {spreadArray: false}],
   [[5151, 23, 781], [Number, Number], false, 'an array with greater length than an array longer than 1', {spreadArray: false}],
+  [['timmy', 13, 403], {_: [Number, String], firstAsStandard: true}, true, 'a longer valid array with an array using firstAsStandard and having specific index formats', {spreadArray: false}],
+  [[['sandwich', 523, 234, 'mayo'], [12, 342234, 243]], {_: [Number, String], firstAsStandard: true}, false, 'a longer array containing an invalid property with an array using firstAsStandard and having specific index formats'],
   [[true, false], [], false, 'booleans as arrays'],
   [null, [], false, 'null as an array'],
   [undefined, [], false, 'undefined as an array'],
