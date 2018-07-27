9953b29767db9f2a7e184dd375e353f16251a35d
L1lith <lily@lillith.pw>
2018-07-26 22:00:21 -0600

    update boolean tests

--- tests/sanitize/boolean.js
+++ tests/sanitize/boolean.js
@@ -4,7 +4,10 @@ const testValid = require('../functions/testValid')
 const expectedMatches = [
   [true, Boolean, true, 'matches true as a boolean'],
   [false, Boolean, true, 'matches false as a boolean'],
-  [[-1, 0, 1], Boolean, false, 'doesn\'t match numbers as booleans']
+  [[-1, 0, 1], Boolean, false, 'doesn\'t match numbers as booleans'],
+  [['', 'tomato'], Boolean, false, 'doesn\'t match strings as booleans'],
+  [{}, Boolean, false, 'doesn\'t match objects as booleans'],
+  [[], Boolean, false, 'doesn\'t match arrays as booleans']
 ]
 
 describe('Boolean Matching', ()=>{
