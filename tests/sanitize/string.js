6c5261bc2dd36c4933f8bcf8d930f4827b3648c8
L1lith <lily@lillith.pw>
2018-07-26 20:22:25 -0600

    update string tests

--- tests/sanitize/string.js
+++ tests/sanitize/string.js
@@ -10,7 +10,10 @@ const expectedMatches = [
   ['SALMON', {_: String, uppercase: true}, true, 'matches uppercase strings with the uppercase flag'],
   ['tuna', {_: String, uppercase: true}, false, 'doesn\'t match lowercase strings for the uppercase flag'],
   ['tricky', {_: String, whitespace: false}, true, 'matches strings without whitespace for whitespace false'],
-  ['fish face', {_: String, whitespace: false}, false, 'doesn\'t match strings with whitespace for whitespace true']
+  ['fish face', {_: String, whitespace: false}, false, 'doesn\'t match strings with whitespace for whitespace true'],
+  ['goku@gmail.com', {_: String, email: true}, true, 'matches email strings flagged as emails'],
+  ['basdfwawfe', {_: String, email: true}, false, 'doesn\'t match random strings as emails'],
+  ['', {_: String, email: true, minLength: 0}, false, 'doesn\'t match empty strings as meails']
 ]
 
 describe('String Matching', ()=>{
