8a8dc72402651eea6a68a2813cbd49eec3ff3b27
L1lith <lily@lillith.pw>
2018-07-27 12:06:49 -0600

    update default username format

--- source/customFormats.js
+++ source/customFormats.js
@@ -1,5 +1,5 @@
 module.exports = {
   email: {_: String, email: true, lowercase: true},
-  username: {_: String, regex: /^[a-zA-Z\-0-9]+$/, minLength: 3, lowercase: true, validate: [str => str.startsWith('-') ? `Username cannot start with "-"` : true, str => str.endsWith('-') ? `Username cannot end with "-"` : true]},
+  username: {_: String, regex: /^[a-zA-Z\-0-9]+$/, minLength: 3, lowercase: true, validate: [str => str.startsWith('-') ? `Username cannot begin with a dash` : true, str => str.endsWith('-') ? `Username cannot end with a dash` : true, str => str.includes('--') ? "Username cannot include consecutive dashes" : true]},
   password: {_: String, minLength: 8}
 }
