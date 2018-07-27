22cf2c2675624d2f664d80b7d8f3afe612a8275b
L1lith <lily@lillith.pw>
2018-07-27 10:41:58 -0600

    write more tests

--- tests/sanitize/object.js
+++ tests/sanitize/object.js
@@ -3,6 +3,12 @@ const testValid = require('../functions/testValid')
 const expected = [
   [{}, {}, true, 'an empty object with an empty object'],
   [{a: 12}, {}, false, 'an object with properties with an empty object'],
+  [{}, {age: Number}, false, 'an object with missing properties'],
+  [{color: 'green'}, {color: String}, true, 'an object with matching properties'],
+  [{sauce: true}, {_:{}, strict: false}, true, 'an object with properties with a non-strict empty object'],
+  [{}, {flavor: {_: String, optional: true}}, true, 'an empty object with an object with an optional property'],
+  [{flavor: 'spicy'}, {flavor: {_: String, optional: true}}, true, 'an object with a valid optoinal property'],
+  [{flavor: Infinity}, {flavor: {_: String, optional: true}}, false, 'an object with an invalid optional property'],
   [true, {}, false, 'true as an object'],
   [false, {}, false, 'false as an object'],
   [null, {}, false, 'null as an object'],
