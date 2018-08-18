51ebc825ad031b07240a1b0b3b261394e38f8511
L1lith <lily@lillith.pw>
2018-08-18 16:35:17 -0600

    improve array format

--- source/sanitize/array.js
+++ source/sanitize/array.js
@@ -2,19 +2,19 @@ const firstError = require('../functions/firstError')
 
 function sanitizeArray(sanitizeAny, input, format, options) {
   if (!Array.isArray(input)) return 'Expected Array'
-  let {firstAsStandard, strict} = options
-  if (firstAsStandard === undefined && format.length === 1) firstAsStandard = true // Default to true only if there's only one entry in the array.
-  if (format.length !== 1 && !options.hasOwnProperty('strict')) strict = true // Default to true only if we're not using firstAsStandard
+  let {firstAsStandard, strict, minLength, maxLength, length} = options
+  if (!options.hasOwnProperty('firstAsStandard') && format.length === 1) firstAsStandard = true // Default to true only if there's only one entry in the array.
   let standard = null
   if (firstAsStandard === true) {
     if (format.length < 1) throw new Error('Missing First Array Value for First As Standard')
     standard = format.splice(0,1)[0]
   }
+  if (!options.hasOwnProperty('strict') && format.length > 0) strict = true // Default to true only if the format length is greater than 0
 
   if (!options.hasOwnProperty('minLength') && format.length > 0) options.minLength = 1
-  if (options.hasOwnProperty('minLength') && input.length < options.minLength) return 'Array Too Short'
-  if (options.hasOwnProperty('maxLength') && input.length > options.maxLength) return 'Array Too Long'
-  if (options.hasOwnProperty('length') && input.length !== options.length) return 'Incorrect Array Length'
+  if (options.hasOwnProperty('minLength') && input.length < minLength) return 'Array Too Short'
+  if (options.hasOwnProperty('maxLength') && input.length > maxLength) return 'Array Too Long'
+  if (options.hasOwnProperty('length') && input.length !== length) return 'Incorrect Array Length'
 
   const errors = []
 
--- source/validate/array.js
+++ source/validate/array.js
@@ -1,17 +1,21 @@
 function validateArrayOptions(options) {
-  const {firstAsStandard, minLength, maxLength, length} = options
+  const {firstAsStandard, minLength, maxLength, length, strict} = options
+  if (options.hasOwnProperty('strict')) {
+    if (typeof strict != 'boolean') return 'Invalid Strict Option, expected a boolean'
+  }
   if (options.hasOwnProperty('minLength')) {
-    if (typeof minLength != 'number' && !isFinite(minLength)) return 'Invalid MinLength Option'
+    if (typeof minLength != 'number' && !isFinite(minLength)) return 'Invalid MinLength Option, expected a number'
   }
   if (options.hasOwnProperty('maxLength')) {
-    if (typeof maxLength != 'number' && !isFinite(maxLength)) return 'Invalid MaxLength Option'
+    if (typeof maxLength != 'number' && !isFinite(maxLength)) return 'Invalid MaxLength Option, expected a number'
   }
   if (options.hasOwnProperty('length')) {
-    if (typeof length != 'number' && !isFinite(length)) return 'Invalid Length Option'
+    if (typeof length != 'number' && !isFinite(length)) return 'Invalid Length Option, expected a number'
   }
   if (options.hasOwnProperty('firstAsStandard')) {
-    if (typeof firstAsStandard != 'boolean') return 'Invalid First As Standard Option'
+    if (typeof firstAsStandard != 'boolean') return 'Invalid First As Standard Option, expected a boolean'
   }
+  if (options.hasOwnProperty('firstAsStandard') && options.hasOwnProperty('strict')) return 'Cannot Use First As Standard and Strict at the same time'
   return null
 }
 
