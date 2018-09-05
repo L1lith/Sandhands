8c885b3398a929262aed19da26df3b8e64c60b59
L1lith <lily@lillith.pw>
2018-09-05 00:41:58 -0600

    add support for deep format shorthand

--- source/functions/interpretFormatShorthand.js
+++ source/functions/interpretFormatShorthand.js
@@ -1,7 +1,15 @@
 const FormatClass = require('../exports/Format').Format
 
 function interpretFormatShorthand(format) {
-  if(format instanceof FormatClass) return format.format
+  if(typeof format == 'object' && format !== null) {
+    if (format instanceof FormatClass) return interpretFormatShorthand(format.format)
+    if (format.hasOwnProperty('_')) return {...format, _: interpretFormatShorthand(format._))}
+    const output = {}
+    Object.entries(format).forEach(([key, value]) => {
+      output[key] = interpretFormatShorthand(value)
+    })
+    return output
+  }
   return format
 }
 
