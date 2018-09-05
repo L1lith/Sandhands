3f13d688b0f30c91070c05eccce3d52f5adf8f35
L1lith <lily@lillith.pw>
2018-09-04 23:49:48 -0600

    Create new Format options shorthand utility.

---
+++ source/exports/Format.js
@@ -0,0 +1,31 @@
+const validate = require('../validate/any')
+
+class Format {
+  constructor(format, options) {
+    validate(format, options)
+    this.format = {...(options || {}), _: format}
+  }
+}
+
+const formatProxy = {
+  get: (target, prop) => {
+    if (!(target instanceof Format)) throw new Error("Expected Format Instance")
+    if (prop === "_") throw new Error("You must assign the format in the constructor")
+    if (prop === "format") return target.format
+    return (newValue=true)=>{
+      target.format[prop] = newValue
+      return new Proxy(target, formatProxy)
+    }
+  },
+  construct(target, args) {
+    return createFormat(...args)
+  }
+}
+
+function createFormat(...args) {
+  if (args.length < 1) throw new Error('Must supply a format.')
+  const [format, options] = args
+  return new Proxy(new Format(format, options), formatProxy)
+}
+
+module.exports = new Proxy(createFormat, formatProxy)
--- source/exports/index.js
+++ source/exports/index.js
@@ -4,5 +4,6 @@ const valid = require('./valid')
 const setDefault = require('./setDefault')
 const customFormat = require('./customFormat')
 const sandhandsExpress = require('./sandhandsExpress')
+const Format = require('./Format')
 
-module.exports = Object.freeze({details, sanitize, valid, setDefault, customFormat, sandhandsExpress})
+module.exports = Object.freeze({details, sanitize, valid, setDefault, customFormat, sandhandsExpress, Format})
