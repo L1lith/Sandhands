b85ee0a9f2da4955f0601c38122bd54abe38ac45
Talha Talip Açıkgöz <talha6851@hotmail.com>
2018-09-13 00:19:04 +0300

    Fixed Production Error
    ```
    implicit@implicit-pc:~/Masaüstü/hexabros/askeri-bileklik-server$ node index.js
    /home/implicit/Masaüstü/hexabros/askeri-bileklik-server/node_modules/sandhands/source/functions/interpretFormatShorthand.js:6
        if (format.hasOwnProperty('_')) return {...format, _: interpretFormatShorthand(format._))}
    ```
    This error shows up on fresh install, there is an extra `)`

--- source/functions/interpretFormatShorthand.js
+++ source/functions/interpretFormatShorthand.js
@@ -3,7 +3,7 @@ const FormatClass = require('../exports/Format').Format
 function interpretFormatShorthand(format) {
   if(typeof format == 'object' && format !== null) {
     if (format instanceof FormatClass) return interpretFormatShorthand(format.format)
-    if (format.hasOwnProperty('_')) return {...format, _: interpretFormatShorthand(format._))}
+    if (format.hasOwnProperty('_')) return {...format, _: interpretFormatShorthand(format._)}
     const output = {}
     Object.entries(format).forEach(([key, value]) => {
       output[key] = interpretFormatShorthand(value)
