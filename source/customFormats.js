e00943be4fe1a6651ba7f123631a293689fccfd2
L1lith <lily@l1lith.com>
2019-05-20 23:36:14 -0600

    add optional integration with is-my-ip-valid for ip validation

--- source/customFormats.js
+++ source/customFormats.js
@@ -1,6 +1,12 @@
-module.exports = {
+const customFormats = {
   email: {_: String, email: true, lowercase: true},
-  username: {_: String, regex: /^[a-zA-Z\-0-9]+$/, minLength: 3, lowercase: true, validate: [str => str.startsWith('-') ? `Username cannot begin with a dash` : true, str => str.endsWith('-') ? `Username cannot end with a dash` : true, str => str.includes('--') ? "Username cannot include consecutive dashes" : true]},
+  username: {_: String, regex: /^[a-zA-Z\-0-9]+$/, trimmed: true, minLength: 3, lowercase: true, validate: [str => str.startsWith('-') ? `Username cannot begin with a dash` : true, str => str.endsWith('-') ? `Username cannot end with a dash` : true, str => str.includes('--') ? "Username cannot include consecutive dashes" : true]},
   password: {_: String, minLength: 8},
   zipCode: {_: String, regex: /(^\d{5}$)|(^\d{5}-\d{4}$)/}
 }
+
+try {
+  const validator = require('is-my-ip-valid')
+  const validate = validator()
+  customFormats.IP = {_: String, validate: str => validate(str)}
+} catch(error) {}
