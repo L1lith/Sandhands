cfbd9356e8c829bd1420a1af7f02909729efe413
L1lith <lily@l1lith.com>
2019-05-20 22:29:25 -0600

    allow manually specifying the array format

--- source/sanitize/array.js
+++ source/sanitize/array.js
@@ -3,8 +3,10 @@ const firstError = require('../functions/firstError')
 function sanitizeArray(sanitizeAny, input, format, options) {
   if (!Array.isArray(input)) return 'Expected Array'
   let {firstAsStandard, strict, minLength, maxLength, length} = options
-  if (!options.hasOwnProperty('firstAsStandard') && format.length === 1) firstAsStandard = true // Default to true only if there's only one entry in the array.
-  let standard = null
+  if (!options.hasOwnProperty('firstAsStandard') && !options.hasOwnProperty('standard') && format.length === 1) firstAsStandard = true // Default to true only if there's only one entry in the array.
+  if (options.firstAsStandard === true && options.hasOwnProperty('standard')) throw new Error("Cannot both specify standard manually and set firstAsStandard to true.")
+  const hasStandard = options.firstAsStandard === true || options.hasOwnProperty('standard')
+  let standard = options.standard || null
   if (firstAsStandard === true) {
     if (format.length < 1) throw new Error('Missing First Array Value for First As Standard')
     standard = format.splice(0,1)[0]
@@ -22,7 +24,7 @@ function sanitizeArray(sanitizeAny, input, format, options) {
     if (format.hasOwnProperty(index)) {
       const inputError = sanitizeAny(inputValue, format[index])
       if (inputError !== null) errors[index] = inputError
-    } else if (firstAsStandard === true) {
+    } else if (hasStandard) {
       const inputError = sanitizeAny(inputValue, standard)
       if (inputError !== null) errors[index] = inputError
     } else if (strict === true) {
