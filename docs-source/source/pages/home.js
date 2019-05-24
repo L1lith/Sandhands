1994e25c96ef0e0138065ce99d340bdaf1b19e8c
L1lith <lily@l1lith.com>
2019-05-24 03:08:06 -0600

    improve the docs home page

--- docs-source/source/pages/home.js
+++ docs-source/source/pages/home.js
@@ -1,13 +1,25 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 class Home extends Component {
-  render() {
-    return (
-      <div className="home">
-        <p>Welcome to the Sandhands Documentation. Use the links above to get started</p>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div className="home">
+                <h2>Getting started with Sandhands</h2>
+                <p>
+                    Sandhands is used to sanitize all kinds of arbitarily complex user input! To get
+                    started try looking around at the{' '}
+                    <li>
+                        <Link to="/Sandhands/format">Format section</Link>
+                    </li>{' '}
+                    and{' '}
+                    <li>
+                        <Link to="/Sandhands/sandbox">play around with the sandbox!</Link>
+                    </li>
+                </p>
+            </div>
+        )
+    }
 }
 
-export default {path: '/Sandhands/', exact: true, component: Home}
+export default { path: '/Sandhands/', exact: true, component: Home }
