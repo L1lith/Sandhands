e72d566785d1aa41697c9e2142ec11cbb3c7cc0b
unknown <lily@l1lith.com>
2019-11-18 11:41:35 -0700

    more bugfixes

--- docs-source/pages/home.js
+++ docs-source/pages/home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Link from 'next/Link'
 import '../styles/home.less'
+import Highlight from 'react-highlight'
 
 class Home extends Component {
     render() {
@@ -17,6 +18,30 @@ class Home extends Component {
                         <Link href="/sandbox.html"><a>play around with the sandbox!</a></Link>
                     </li>
                 </p>
+                <h2>A Basic Express Example</h2>
+              <Highlight className="javascript example">{`const express = require('express')
+const {sandhandsExpress} = require('sandhands')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+app.use(bodyParser.json())
+
+app.post('/register', sandhandsExpress({
+  username: 'username',
+  email: 'email',
+  password: 'password'
+}), (req, res) => {
+  console.log('Got Registration Details', req.body)
+  res.send('Registered')
+})
+
+const port = 8050
+
+app.listen(port, err => {
+  if (err) return console.log(err)
+  console.log(\`Server running on Port #\$\{port\}\`)
+})`}</Highlight>
             </div>
         )
     }
